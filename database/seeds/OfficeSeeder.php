4c307869e1b44f11171298c32a3d7b5c3580f3d9
Fercho Posting <shaaf4ever@gmail.com>
2021-07-06 16:24:46 -0500

    Added office factory

---
+++ database/seeds/OfficeSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+use App\Office;
+use Illuminate\Database\Seeder;
+
+class OfficeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        factory(Office::class, 4)->create();
+        factory()
+    }
+}
