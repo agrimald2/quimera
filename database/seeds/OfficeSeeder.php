b32c6c665976aca6c0592cbe790ac2af7c6532cb
Fercho Posting <shaaf4ever@gmail.com>
2021-07-06 16:52:11 -0500

    Seeder creado

--- database/seeds/OfficeSeeder.php
+++ database/seeds/OfficeSeeder.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Brand;
+use App\Category;
+use App\Collection;
+use App\Color;
 use App\Office;
+use App\Product;
+use App\Size;
 use Illuminate\Database\Seeder;
 
 class OfficeSeeder extends Seeder
@@ -12,7 +18,35 @@ class OfficeSeeder extends Seeder
      */
     public function run()
     {
-        factory(Office::class, 4)->create();
-        factory()
+        Office::create([
+            'name' => "Oficina central",
+        ]);
+        Office::create([
+            'name' => "Oficina de abajo",
+        ]);
+
+        Category::create([
+            'name' => 'Categoria maestra',
+        ]);
+
+        Collection::create([
+            'name' => 'Coleccion maestra',
+        ]);
+
+        Brand::create([
+            'name' => 'MARCA 1'
+        ]);
+
+        Color::create([
+            'name' => 'Rojo',
+            'hex' => '#ff0000',
+        ]);
+
+
+        Size::create([
+            'number' => 12,
+            'name' => 'talla',
+        ]);
+
     }
 }
