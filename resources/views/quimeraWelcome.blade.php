b8cb98491b58548e7ef248ede8afa89cd5dd74e0
Fercho Posting <shaaf4ever@gmail.com>
2021-07-07 13:21:00 -0500

    Categories

--- resources/views/quimeraWelcome.blade.php
+++ resources/views/quimeraWelcome.blade.php
@@ -26,11 +26,12 @@
                 </div>
             </section>
             <section class="types">
+                <div class="typesBg"></div>
                 <div class="d-flex flex-column typeScroll">
                     <label class="text-center keep-exp" for="card_name">SIGUE EXPLORANDO</label>
                     <div class="d-flex typeScrollHidden">
                         @forelse ($categories as $row)
-                            <div class="typeScrollImg" style=" background-color: white; margin-right: 10px,; width: 24%;height: 168px;">
+                            <div class="typeScrollImg" style=" background-color: white; margin-right: 10px,; width: 24%;">
                                 <a href="/store/{{$row['id']}}" class="d-flex flex-column">
                                     @if ($row['image_url'] != null)
                                         <img src="/api/products/{{$row['image_url']}}" style="padding: 5px;width: 100%">
