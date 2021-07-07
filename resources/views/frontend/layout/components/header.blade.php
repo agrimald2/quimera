315fdcb8d9d5f9db91ff53342b924c9ff8a0ffca
Fercho Posting <shaaf4ever@gmail.com>
2021-07-07 18:52:40 -0500

    Convert header to tailwind

--- resources/views/frontend/layout/components/header.blade.php
+++ resources/views/frontend/layout/components/header.blade.php
@@ -1,3 +1,3 @@
-<header class="header" id="header" align="center">
+<div class="bg-red-100 px-4 text-center font-bold py-4">
   ENVÍO GRATIS A TODO EL PERÚ / COVID 19: ENTREGAS SIN CONTACO / NEW IN 20% OFF CODIGO: NEWIN20
-</header>
+</div>
