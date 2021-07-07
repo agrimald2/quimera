184df1d686c169763c41ac1e4835f19ee4e10192
Fercho Posting <shaaf4ever@gmail.com>
2021-07-07 13:39:39 -0500

    Big text

--- resources/views/quimeraWelcome.blade.php
+++ resources/views/quimeraWelcome.blade.php
@@ -80,7 +80,7 @@
                 </div>  
             </section>
             <section class="join-us d-flex justified-content-center flex-column">
-                <label class="text-center" for="card_name" style="padding: 20px;font-family: 'Cedarville Cursive' ,cursive;font-size: 19px;">Se parte de nuestra comunidad en Instagram</label>
+                <label class="text-center" for="card_name" style="padding: 20px;font-family: 'Cedarville Cursive' ,cursive;font-size: 27px;">Se parte de nuestra comunidad en Instagram</label>
                 <div class="d-flex">
                     <div class="w-100">
                         <img class="comunity-img-desk" src="./assets/images/quimera/info.png" style="width: 100%">
