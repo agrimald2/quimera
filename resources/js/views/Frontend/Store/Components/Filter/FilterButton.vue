2bf4c3b3ba4f652a18c3cf9cda1ab13696db602f
Fercho Posting <shaaf4ever@gmail.com>
2021-07-08 15:12:16 -0500

    Added filter button

---
+++ resources/js/views/Frontend/Store/Components/Filter/FilterButton.vue
@@ -0,0 +1,23 @@
+<template>
+<div class="border-b-2 border-red-100">
+  
+  <div class="name cursor-pointer uppercase bg-red-100 hover:bg-red-100 px-2 py-4">
+    {{ name }}
+  </div>
+  
+  <div class="items grid pt-4 pb-4">
+    <slot />
+  </div>
+
+</div>
+</template>
+
+
+<script>
+export default {
+  props: {
+    name: String,
+  },
+
+}
+</script>
