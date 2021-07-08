5ceb553378127a03fb32063fbb9f2b24d4347e7e
Fercho Posting <shaaf4ever@gmail.com>
2021-07-08 16:13:03 -0500

    Active filter button color

--- resources/js/views/Frontend/Store/Components/Filter/FilterButton.vue
+++ resources/js/views/Frontend/Store/Components/Filter/FilterButton.vue
@@ -2,8 +2,10 @@
 <div class="border-b-2 border-red-100 text-black">
   
   <div
-    class="name cursor-pointer uppercase bg-red-100 hover:bg-red-100 px-2 py-3"
-    @click="visible = !visible">
+    class="name cursor-pointer uppercase hover:bg-red-100 px-2 py-3"
+    @click="visible = !visible"
+    :class="{'bg-red-100': visible}"
+    >
     {{ name }}
   </div>
   
