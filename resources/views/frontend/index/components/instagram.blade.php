cb74eb803f631ca8837f7d5bc9a03b366737e72c
Fercho Posting <shaaf4ever@gmail.com>
2021-07-07 19:09:43 -0500

    Removed #page and #app from blade views

---
+++ resources/views/frontend/index/components/instagram.blade.php
@@ -0,0 +1,9 @@
+<section class="join-us d-flex justified-content-center flex-column">
+  <label class="text-center" for="card_name" style="padding: 20px;font-family: 'Cedarville Cursive' ,cursive;font-size: 27px;">Se parte de nuestra comunidad en Instagram</label>
+  <div class="d-flex">
+    <div class="w-100">
+      <img class="comunity-img-desk" src="./assets/images/quimera/info.png" style="width: 100%">
+      <img class="comunity-img-mobile" src="./assets/images/quimera/comunity.png" style="width: 100%">
+    </div>
+  </div>
+</section>
--- resources/views/frontend/index/index.blade.php
+++ resources/views/frontend/index/index.blade.php
@@ -2,55 +2,46 @@
 
 
 @section('content')
-<div id="page">
-    <div id="app" class="d-flex flex-column">
+<div>
+  <div class="d-flex flex-column">
     <label class="text-center" for="card_name" style="font-family: 'Cedarville Cursive' ,cursive;font-size: 29px;">Sometimes minimal, Sometimes Boho.. Always me.</label>
-        @include('frontend.index.components.brands')
-        
-        @include('frontend.index.components.types')
+    
+    @include('frontend.index.components.brands')
 
-        <!-- @include('frontend.index.components.newInStore') -->
-        
-        <section class="join-us d-flex justified-content-center flex-column">
-            <label class="text-center" for="card_name" style="padding: 20px;font-family: 'Cedarville Cursive' ,cursive;font-size: 27px;">Se parte de nuestra comunidad en Instagram</label>
-            <div class="d-flex">
-                <div class="w-100">
-                    <img class="comunity-img-desk" src="./assets/images/quimera/info.png" style="width: 100%">
-                    <img class="comunity-img-mobile" src="./assets/images/quimera/comunity.png" style="width: 100%">
-                </div>
-            </div>
-        </section>
-    </div>
+    @include('frontend.index.components.types')
+
+    @include('frontend.index.components.newInStore')
+
+    @include('frontend.index.components.instagram')
+  </div>
 </div>
+
 <div class="social">
-    <a class="shopcart-icon" href="/cart">
-        <img src="./assets/images/quimera/car.png" style="width: 100%">
-        <span class="count">
-        <p><label id="count"></label> </p>
-        </span>
-    </a>
+  <a class="shopcart-icon" href="/cart">
+    <img src="./assets/images/quimera/car.png" style="width: 100%">
+    <span class="count">
+      <p><label id="count"></label> </p>
+    </span>
+  </a>
 </div>
 
 
 <script>
-
-$(document).ready(function(){
+  $(document).ready(function() {
     var count_storage = JSON.parse(localStorage.getItem('carts'));
     var count = count_storage.length;
-    document.getElementById('count').innerHTML=count;
+    document.getElementById('count').innerHTML = count;
     console.log(count);
-});
-
+  });
 </script>
 
 
 <style>
-
-.btn-red{
+  .btn-red {
     background: red;
-}
+  }
 
-.social{
+  .social {
     color: #f3ecec;
     width: 56px;
     padding: 14px;
@@ -61,17 +52,18 @@ $(document).ready(function(){
     background: #e0d4d4;
     border-radius: 10px;
     z-index: 1000;
-}
-.social .shopcart-icon {
+  }
+
+  .social .shopcart-icon {
     font-size: 15px;
     position: relative;
     text-transform: uppercase;
     line-height: 6px;
     color: #191717;
     padding-top: 13px;
-}
+  }
 
-.social .count {
+  .social .count {
     position: absolute;
     display: inline-block;
     text-align: center;
@@ -83,8 +75,6 @@ $(document).ready(function(){
     color: #ffffff;
     font-size: 17px;
     line-height: 22px;
-}
-
+  }
 </style>
 @endsection
-
